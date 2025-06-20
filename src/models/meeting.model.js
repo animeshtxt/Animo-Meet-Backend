29feb742b4dea5e975a6dc547cdd116212ad5716
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2025-06-20 20:28:07 +0530

    added meeting and user models

---
+++ src/models/meeting.model.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const MeetingSchema = new Schema({
+  user_id: { type: String },
+  meetingCode: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
+});
+
+const Meeting = mongoose.model("Meeting", MeetingSchema);
+export { Meeting };
