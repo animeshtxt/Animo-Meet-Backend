472eba5a9f09c42387b77ba22cc89aaef286110b
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2025-06-20 20:28:56 +0530

    added user controllers

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,56 @@
+import { User } from "../models/user.model.js";
+import { status } from "http-status";
+import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+  console.log(`name: ${name} \nusername : ${username} \npassword: ${password}`);
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(status.FOUND).json({ message: "User already exists" });
+    }
+
+    const hashedPW = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      username,
+      password: hashedPW,
+    });
+
+    await newUser.save();
+    res
+      .status(status.CREATED)
+      .json({ message: "User registered successfully" });
+  } catch (e) {
+    console.log(`Error in register route : \nERROR = \n ${e}`);
+    return res.status(500).json({ message: `${e}` });
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please provide all credentials" });
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(status.NOT_FOUND).json({ message: "User not found" });
+    }
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+      user.token = token;
+      await user.save();
+      return res.status(status.OK).json({ token: token });
+    }
+  } catch (e) {
+    console.log(`Error in login route : \nERROR = \n ${e}`);
+    return res.status(500).json({ message: `${e}` });
+  }
+};
+
+export { login, register };
