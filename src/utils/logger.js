a04a01e6ebe1b5bb6321ffe7fe9db765c8d6e55c
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2026-04-05 23:05:55 +0530

    feat: add timestamp in logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,24 +3,31 @@ dotenv.config();
 
 const isProd = process.env.NODE_ENV === "production";
 
+const getISTTimestamp = () => {
+  return new Date().toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    hour12: true,
+  });
+};
+
 const logger = {
   // Only prints in Development
   dev: (...args) => {
-    if (!isProd) console.log(...args);
+    if (!isProd) console.log(`I ST[${getISTTimestamp()}] DEV:`, ...args);
   },
   // Only warn in Development
   warn: (...args) => {
-    if (!isProd) console.warn(...args);
+    if (!isProd) console.warn(`[IST ${getISTTimestamp()}] WARN:`, ...args);
   },
 
   // Always prints (Use for critical tracking)
   info: (...args) => {
-    console.log(...args);
+    console.log(`[IST ${getISTTimestamp()}] INFO:`, ...args);
   },
 
   // Always prints errors
   error: (...args) => {
-    console.error(...args);
+    console.error(`[IST ${getISTTimestamp()}] ERROR:`, ...args);
   },
 };
 
